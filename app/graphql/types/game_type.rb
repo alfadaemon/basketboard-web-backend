2f5907e55cf694716eaced0acc8457ec89c1a2dc
Luis Araya <lf.araya@gmail.com>
2023-08-09 17:13:15 -0600

    Add query to get the games in a tournament

--- app/graphql/types/game_type.rb
+++ app/graphql/types/game_type.rb
@@ -3,13 +3,21 @@
 module Types
   class GameType < Types::BaseObject
     field :id, ID, null: false
-    field :team1_id, Integer, null: false
-    field :team2_id, Integer, null: false
+    field :local, TeamType
+    field :away, TeamType
     field :team1_score, Integer, null: false
     field :team2_score, Integer, null: false
-    field :tournament, GraphQL::Types::TournamentType
+    field :tournament, TournamentType
     field :on_date, GraphQL::Types::ISO8601Date
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def local
+      object.team1
+    end
+    
+    def away
+      object.team2
+    end
   end
 end
