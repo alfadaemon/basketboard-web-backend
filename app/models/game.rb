306df7d4068f85a244dd356aef02d6d2971f88f3
Luis Araya <lf.araya@gmail.com>
2023-08-14 17:02:30 -0600

    Refactor Game model and graphql type

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,16 +1,16 @@
 class Game < ApplicationRecord
   belongs_to :tournament
-  belongs_to :team1, class_name: 'Team'
-  belongs_to :team2, class_name: 'Team'
+  belongs_to :local_team, class_name: 'Team'
+  belongs_to :away_team, class_name: 'Team'
   
   after_validation :must_be_in_same_tournament, on: :create
 
-  def team1_name
-    self.team1.name
+  def local_team_name
+    self.local.name
   end
 
-  def team2_name
-    self.team2.name
+  def away_team_name
+    self.away.name
   end
 
   private
